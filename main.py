95fd07357477940dcbea48cce8911e1128fd5b66
Gorilon2000 <avillac@okstate.edu>
2025-09-15 20:29:21 -0500

    Descripcion de Estadisticas basicas

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
+df=pd.read_csv('3. ciclismo.csv')
+
+df.describe()
