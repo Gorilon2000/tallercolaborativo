444e3f62cc5263666d3a8999ed64a2398e2e70e8
Gorilon2000 <avillac@okstate.edu>
2025-09-17 22:28:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import warnings
+import w
+import seaborn as sns
 warnings.filterwarnings('ignore')
 
 df=pd.read_csv('3. ciclismo.csv')
 
 df.describe()
+
+sns.pairplot(df)
